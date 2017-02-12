53db3440f8892a973da04f47c6dd040f7992ff00
dsengmanivanh <daniel.sengmanivanh@gmail.com>
2017-02-12 20:31:54 +0100

    [Prairie-cqp]Exercise

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class Calculator {
+
+    public int addition(int i, int i1) {
+        return 0;
+    }
+
+    public int subtraction(int i, int i1) {
+        return 0;
+    }
+
+    public float divide(int i, int i1) {
+        return 0;
+    }
+}
