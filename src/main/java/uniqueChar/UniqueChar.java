9220dcddb4664533241a34ca5eb22c30d07c21c7
dsengmanivanh <daniel.sengmanivanh@gmail.com>
2017-02-27 15:31:05 +0100

    [Prairie-cqp]UniqueChar other added

--- src/main/java/uniqueChar/UniqueChar.java
+++ src/main/java/uniqueChar/UniqueChar.java
@@ -14,4 +14,19 @@ public class UniqueChar {
         return containsUnique;
 
     }
+
+    public boolean isUniqueCharOther(String asciiStr) {
+        if (asciiStr == null || asciiStr.isEmpty()) {
+            return false;
+        }
+        boolean[] charSet = new boolean[256];
+        for (int i = 0; i < asciiStr.length(); i++) {
+            int asciiVal = asciiStr.charAt(i);
+            if (charSet[asciiVal]) {
+                return false;
+            }
+            charSet[asciiVal] = true;
+        }
+        return true;
+    }
 }
